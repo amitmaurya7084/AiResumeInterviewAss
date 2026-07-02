63b7841aa8242cfde67bce1afb5c5c0219e89b46
Amit maurya <amitmaurya49135@gmail.com>
2026-07-02 11:33:25 +0530

    update firebase config

--- client/src/utils/firebase.js
+++ client/src/utils/firebase.js
@@ -3,11 +3,11 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
     apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
-    authDomain: "interviewiq-a2b8b.firebaseapp.com",
-    projectId: "interviewiq-a2b8b",
-    storageBucket: "interviewiq-a2b8b.firebasestorage.app",
-    messagingSenderId: "973724831200",
-    appId: "1:973724831200:web:f7043218eb97f2c50d2f85"
+    authDomain: "interview-assistant-43d25.firebaseapp.com",
+    projectId: "interview-assistant-43d25",
+    storageBucket: "interview-assistant-43d25.firebasestorage.app",
+    messagingSenderId: "928839257686",
+    appId: "1:928839257686:web:f389f22e1735ffad43b5f7"
 };
 
 const app = initializeApp(firebaseConfig);
